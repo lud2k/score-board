8adc1eb45526dc82ea6afb52e8508ed84293e61d
Ludovic Cabre <ludovic@knewton.com>
2018-01-25 22:08:04 -0500

    Fix rankings overflow.

--- src/view/ranking.tsx
+++ src/view/ranking.tsx
@@ -14,26 +14,28 @@ export class Ranking extends React.Component<{ data: Data, stats: Stats }, {}> {
     const minRating = rankings.length > 0 ? _.last(rankings).rating : 0
 
     return (
-      <Table className={styles.ranking}>
-        <TableHead>
-          <TableRow>
-            <TableCell style={{ paddingRight: 24 }} numeric>Rank</TableCell>
-            <TableCell style={{ width: '100%' }}>Player</TableCell>
-            <TableCell numeric>Rating</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rankings.map((ranking: PlayerRanking, index: number) => (
-            <TableRow key={ranking.playerId}>
-              <TableCell style={{ paddingRight: 24, textAlign: 'center' }} numeric>
-                {index+1}
-              </TableCell>
-              <TableCell>{data.players[ranking.playerId].name}</TableCell>
-              <TableCell numeric>{(ranking.rating-minRating).toFixed(2)}</TableCell>
+      <div className={styles.ranking}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ paddingRight: 24 }} numeric>Rank</TableCell>
+              <TableCell style={{ width: '100%' }}>Player</TableCell>
+              <TableCell numeric>Rating</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {rankings.map((ranking: PlayerRanking, index: number) => (
+              <TableRow key={ranking.playerId}>
+                <TableCell style={{ paddingRight: 24, textAlign: 'center' }} numeric>
+                  {index+1}
+                </TableCell>
+                <TableCell>{data.players[ranking.playerId].name}</TableCell>
+                <TableCell numeric>{(ranking.rating-minRating).toFixed(2)}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     )
   }
 }
